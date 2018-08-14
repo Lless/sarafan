32aca026ede1d0708324b3071984e3896266f190
Lless <igorsan1@mail.ru>
2018-08-14 21:42:03 +0300

    create "message" rest service

---
+++ src/main/java/com/vk/id26639136/sarafan/Exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.vk.id26639136.sarafan.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+}
---
+++ src/main/java/com/vk/id26639136/sarafan/SarafanApplication.java
@@ -0,0 +1,12 @@
+package com.vk.id26639136.sarafan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarafanApplication.class, args);
+	}
+}
