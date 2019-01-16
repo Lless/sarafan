2179f20fff53266abe586b8df759e78621dd7c9a
Lless <igorsan1@mail.ru>
2019-01-16 22:56:38 +0300

    add styles with vuetify

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from 'pages/app.vue'
 import VueResource from 'vue-resource'
 import { connect } from './util/ws'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 if (frontendData.profile) {
     connect()
 }
+
+Vue.use(Vuetify)
 Vue.use(VueResource)
 
 new Vue({
